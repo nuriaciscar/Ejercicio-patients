83d55b8b05f4867c35ddd752c685f94ae5eda149
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-08 13:39:06 +0200

    Added initial files

---
+++ ejercicio-pacientes.js
@@ -0,0 +1,56 @@
+const pacientes = new Array(
+  {
+    paciente: {
+      nombre: "Manuel",
+      edad: 25,
+      sexo: "H",
+    },
+    diasIngresado: 3,
+    dieta: "Baja en fibra",
+  },
+  {
+    paciente: {
+      nombre: "Marta",
+      edad: 56,
+      sexo: "M",
+    },
+    diasIngresado: 5,
+    dieta: "Diabetes",
+  },
+  {
+    paciente: {
+      nombre: "Julia",
+      edad: 38,
+      sexo: "M",
+    },
+    diasIngresado: 1,
+    dieta: "Sin sal",
+  },
+  {
+    paciente: {
+      nombre: "Esteban",
+      edad: 40,
+      sexo: "H",
+    },
+    diasIngresado: 3,
+    dieta: "Diabetes",
+  },
+  {
+    paciente: {
+      nombre: "Arturo",
+      edad: 19,
+      sexo: "H",
+    },
+    diasIngresado: 2,
+    dieta: "Baja en fibra",
+  },
+  {
+    paciente: {
+      nombre: "Isabel",
+      edad: 63,
+      sexo: "M",
+    },
+    diasIngresado: 6,
+    dieta: "Sin sal",
+  }
+);
